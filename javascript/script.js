a32d28f632c4826e5b9a14958dfc1f6b9799f675
RapiraX <hmumuziev@gmaiil.com>
2024-12-19 12:06:12 +0600

    added changes in map

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,15 @@
-
 ymaps.ready(init);
 function init(){
     let myMap = new ymaps.Map("map", {
-        center: [55.76, 37.64],
-        zoom: 7
+        center: [59.937738, 30.309158],
+        zoom: 16
     });
+    map.controls.remove('geolocationControl'); // удаляем геолокацию
+    map.controls.remove('searchControl'); // удаляем поиск
+    map.controls.remove('trafficControl'); // удаляем контроль трафика
+    map.controls.remove('typeSelector'); // удаляем тип
+    map.controls.remove('fullscreenControl'); // удаляем кнопку перехода в полноэкранный режим
+    map.controls.remove('zoomControl'); // удаляем контрол зуммирования
+    map.controls.remove('rulerControl'); // удаляем контрол правил
+    map.behaviors.disable(['scrollZoom']); // отключаем скролл карты (опционально)
 }
